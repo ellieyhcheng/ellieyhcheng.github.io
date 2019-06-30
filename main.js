ae674a8a3032b735633afccd76f9b80e06a69375
ellie <ellieyhc@gmail.com>
2019-06-30 12:11:51 -0700

    Portfolio and Resume Basics

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const panels = document.querySelectorAll(".panel");
 panels.forEach(panel => {
     const navbarHeight = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--navbar-height'));
-    const panelHeight = window.innerHeight - navbarHeight;
-    const top = panel.dataset.index * panelHeight + navbarHeight;
+    const panelHeight = 670;
+    const top = panel.dataset.index * (panelHeight) + navbarHeight;
     panel.style.height = panelHeight + 'px';
     panel.style.top = top + 'px';
 })
+
+// const projects = document.querySelectorAll('.project');
+// projects.forEach(project => {
+//     const top = project.dataset.index * (400) + 100;
+//     project.style.top = top + 'px';
+
+// })
